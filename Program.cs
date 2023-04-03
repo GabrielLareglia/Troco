5e55dd094ebd4950593f99225b1187d1fca1fce3
GabrielLareglia <laregliagabriel@gmail.com>
2023-04-02 22:07:22 -0300

    terminei

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿decimal valorDoCarro, valorPago, valorJuros;
+
+Console.WriteLine("--- Troco ---\n");
+
+Console.Write("Valor da carro (R$)...: ");
+valorDoCarro = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Valor Pago (R$)........: ");
+valorPago = Convert.ToDecimal(Console.ReadLine());
+
+valorJuros = valorPago - valorDoCarro;
+
+Console.WriteLine($"\nJuros: {valorJuros:C}");
